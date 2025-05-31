558c576060084c2e67d2e7276df0b177f8bd8ca3
Teodor Golisharski <t.golisharski@gmail.com>
2025-05-31 23:53:46 +0300

    delete comments

--- OOP_HW/OOP_HW.cpp
+++ OOP_HW/OOP_HW.cpp
@@ -4,12 +4,6 @@
 
 int main()
 {
-    /*Message m1(1,2, "Teodor beshe tuk");
-    Message m2(2,1, "Teodor ne beshe tuk");
-
-    std::cout << m1.getContent() << " " << m1.getTimeStamp() << std::endl;
-    std::cout << m2.getContent() << " " << m2.getTimeStamp() << std::endl;*/
-
     SystemEngine::run();
 
     return 0;
